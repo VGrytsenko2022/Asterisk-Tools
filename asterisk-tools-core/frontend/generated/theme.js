bcb8a16e49a8ad68ac2e147c90b18174a7661b34
Valentyn Grytsenko <vallico@ukr.net>
2023-08-25 21:34:46 +0300

    25.08.2023 20:55 - Start this project.

---
+++ asterisk-tools-core/frontend/generated/theme.js
@@ -0,0 +1,2 @@
+import {applyTheme as _applyTheme} from './theme-asterisktools.generated.js';
+export const applyTheme = _applyTheme;
--- asterisk-tools-core/src/main/java/ua/vg/at/core/views/about/AboutView.java
+++ asterisk-tools-core/src/main/java/ua/vg/at/core/views/about/AboutView.java
@@ -1,5 +1,6 @@
 package ua.vg.at.core.views.about;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -14,23 +15,30 @@ import ua.vg.at.core.views.MainLayout;
 @Route(value = "about", layout = MainLayout.class)
 @RolesAllowed("ADMIN")
 public class AboutView extends VerticalLayout {
-
     public AboutView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        Image img = new Image("images/PidginBird.png", "placeholder plant");
         img.setWidth("200px");
         add(img);
 
-        H2 header = new H2("This place intentionally left empty");
+        H2 header = new H2("Asterisk - control functions remotely");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        add(new Paragraph("Software developer: Valentyn Grytsenko, vallico@ukr.net, Ukraine Kyiv 2023 🤗"));
+
+        Button donateButton = new Button("Help this project");
+        donateButton.addClickListener(e -> this.openPaypal());
+        add(donateButton);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
+    }
+
+    private void openPaypal() {
+        getUI().get().getPage().open("https://www.paypal.com/donate/?hosted_button_id=GRQBC554NA356", "_blank");
     }
 
 }
